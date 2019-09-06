34fdf9ce7ed07c0e13c86aa39e81689299102add
Pelle Garbus <p@garbus.dk>
2019-09-06 11:38:41 +0200

    setup as in video

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -4,10 +4,9 @@ namespace GradeBook.GradeBooks
 {
   public class RankedGradeBook : BaseGradeBook
   {
-    public GradeBookType _gradeBookType;
     public RankedGradeBook(string name) : base(name)
     {
-      _gradeBookType = GradeBookType.Ranked;
+      Type = GradeBookType.Ranked;
     }
   }
 }
--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -4,11 +4,9 @@ namespace GradeBook.GradeBooks
 {
   public class StandardGradeBook : BaseGradeBook
   {
-    private GradeBookType _gradeBookType;
-
     public StandardGradeBook(string name) : base(name)
     {
-      _gradeBookType = GradeBookType.Standard;
+      Type = GradeBookType.Standard;
     }
   }
 }
